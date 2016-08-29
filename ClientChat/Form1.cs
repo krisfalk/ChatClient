a4c3b3c77e2d7b6e56246bffdfe1346613cde78b
krisfalk <krisfalk2007@hotmail.com>
2016-08-29 16:13:07 -0500

    initial upload of chat client

---
+++ ClientChat/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace ChatClientGUI
+{
+    public partial class Form1 : Form
+    {
+        private const int portNum = 15000;
+        delegate void SetTextCallback(string text);
+
+        TcpClient client;
+        NetworkStream ns;
+        Thread t = null;
+        private const string hostName = "localhost";
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new TcpClient(hostName, portNum);
+            ns = client.GetStream();
+            String s = "Connected";
+            byte[] byteTime = Encoding.ASCII.GetBytes(s);
+            ns.Write(byteTime, 0, byteTime.Length);
+            t = new Thread(DoWork);
+            t.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s = textBox2.Text;
+            byte[] byteTime = Encoding.ASCII.GetBytes(s);
+            ns.Write(byteTime, 0, byteTime.Length);
+        }
+
+        // This is run as a thread
+        public void DoWork()
+        {
+            byte[] bytes = new byte[1024];
+            while (true)
+            {
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+                this.SetText(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+                //MessageBox.Show(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+            }
+        }
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox1.Text = this.textBox1.Text + text;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
